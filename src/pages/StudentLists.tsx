e4aaeeb0fdd5042bdcf4b968a8f499babeff1feb
kennethlili <lichikin1997@gmail.com>
2024-05-08 11:14:47 +0800

    feat: tailwind setup

---
+++ src/pages/StudentLists.tsx
@@ -0,0 +1,11 @@
+export function StudentLists() {
+  return (
+    <div className="text-red-500">
+      <h1>Student Lists</h1>
+      <p>Student 1</p>
+      <p>Student 2</p>
+      <p>Student 3</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
